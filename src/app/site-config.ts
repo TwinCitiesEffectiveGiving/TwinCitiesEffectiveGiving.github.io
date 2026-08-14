29efd7d92301eccd2b3ff98ab9632cd7d06c82cd
Daniel Turnquist <turnq005@gmail.com>
2026-08-14 15:04:20 -0500

    Add Google Calendar API key

--- src/app/site-config.ts
+++ src/app/site-config.ts
@@ -7,7 +7,7 @@ export const siteConfig = {
   email: 'hello@twincitieseffectivegiving.org',
   address: 'Twin Cities Metro, MN',
   calendarId: '890fe32dd1ab8fe27c6b1acc17153695630951e5f19bcb29614172da8e3b5d03@group.calendar.google.com',
-  googleCalendarApiKey: '',
+  googleCalendarApiKey: 'AIzaSyBClO35VDgxCaFvamJD2X4VxCEQ7v44S84',
   calendarEmbedUrl:
     'https://calendar.google.com/calendar/embed?src=890fe32dd1ab8fe27c6b1acc17153695630951e5f19bcb29614172da8e3b5d03%40group.calendar.google.com&ctz=America%2FChicago',
   socials: [
